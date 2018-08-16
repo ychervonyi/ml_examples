2d73148196efaa7c4a1fa92c8d86be1eb366d4f0
yuri <ychervonyi@gmail.com>
2018-08-16 15:46:56 -0700

    Refactored college linear regresssion model

--- college_scorecard/model.py
+++ college_scorecard/model.py
@@ -1,10 +1,25 @@
-import seaborn as sns
+'''
+features = ['SAT_AVG_ALL']
+
+Test loss: 49939391.2836
+Features: ['SAT_AVG_ALL']
+Number of examples: 9826
+
+features = ['SAT_AVG_ALL', 'SATVRMID', 'SATMTMID', 'SATWRMID']
+
+Test loss: 68742027.3115
+Features: ['SAT_AVG_ALL', 'SATVRMID', 'SATMTMID', 'SATWRMID']
+Number of examples: 3253
+
+'''
+
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 # from keras.regularizers import l2
 # from keras.utils import np_utils
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 import keras
 import pandas
 import numpy as np
@@ -32,8 +47,11 @@ def get_data(features):
 
     cwd = os.getcwd()
     files = sorted(glob.glob(cwd + '/CollegeScorecard_Raw_Data/*.csv'))
-    files = [cwd + '/CollegeScorecard_Raw_Data/MERGED2013_14_PP.csv']
+    # files = [cwd + '/CollegeScorecard_Raw_Data/MERGED2013_14_PP.csv']
     for file in files:
+        # Consider only data from 2xxx (previous years don't have the data we are interested in)
+        if file.split('/')[-1][6:-7].startswith('1'):
+            continue
         df = pandas.read_csv(file, delimiter=',', error_bad_lines=False)
         # Extract year from file name
         year = file.split('/')[-1][6:-7]
@@ -77,36 +95,49 @@ def get_data(features):
     return data_filtered
 
 
-features = ['SAT_AVG_ALL', 'SATVRMID', 'SATMTMID', 'SATWRMID']
-n_features = len(features)
+def train_model(features):
+    n_features = len(features)
 
-dataset = get_data(features)
-data = dataset['2013_14']
-X, Y = data[:, :n_features], data[:, -1:]
+    dataset = get_data(features)
+    data = None
+    for key, value in dataset.items():
+        if data is None:
+            data = value
+        else:
+            data = np.concatenate((data, value), axis=0)
 
-# Make test and train set
-train_X, test_X, train_y, test_y = train_test_split(X, Y, train_size=0.7, random_state=0)
+    # data = dataset['2013_14']
+    X, Y = data[:, :n_features], data[:, -1:]
 
-# Linear regression
-model = Sequential()
-model.add(Dense(1, input_shape=(n_features,), activation="linear"))
-model.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=0.01))
-print(model.summary())
+    # Make test and train set
+    train_X, test_X, train_y, test_y = train_test_split(X, Y, train_size=0.7, random_state=0)
 
-# train
-history = model.fit(train_X, train_y,
-                    batch_size=16,
-                    epochs=300,
-                    verbose=1,
-                    validation_data=(test_X, test_y))
+    # Linear regression
+    model = Sequential()
+    model.add(Dense(1, input_shape=(n_features,), activation="linear"))
+    model.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=0.1))
+    print(model.summary())
 
-# Evaluate and print MSE
-score = model.evaluate(test_X, test_y, verbose=0)
-print('Test loss: %.4f' % score)
-print("Features: %s" % features)
-print("Number of examples: %d" % len(Y))
+    # train
+    history = model.fit(train_X, train_y,
+                        batch_size=16,
+                        epochs=300,
+                        verbose=1,
+                        validation_data=(test_X, test_y))
 
-# # Regression plot
-# ax = sns.regplot(x='X', y='Y', data=pandas.DataFrame({'X': test_X, 'Y': test_y}))
-# ax.set(xlabel='Average SAT', ylabel='Mean earnings 6 years after entry')
-# plt.show()
+    # Evaluate and print MSE
+    score = model.evaluate(test_X, test_y, verbose=0)
+    print('Test loss: %.4f' % score)
+    print("Features: %s" % features)
+    print("Number of examples: %d" % len(Y))
+
+    # # Regression plot
+    # ax = sns.regplot(x='X', y='Y', data=pandas.DataFrame({'X': test_X, 'Y': test_y}))
+    # ax.set(xlabel='Average SAT', ylabel='Mean earnings 6 years after entry')
+    # plt.show()
+
+    return score
+
+if __name__ == '__main__':
+    features = ['SAT_AVG_ALL', 'SATVRMID', 'SATMTMID'] #, 'SATWRMID']
+    score = train_model(features)
