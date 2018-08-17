8e52035af0742270f4260eaa7f0e22eba2a813b9
yuri <ychervonyi@gmail.com>
2018-08-17 15:11:18 -0700

    Added college model, added sklearn model, added saving and loading

--- college_scorecard/model.py
+++ college_scorecard/model.py
@@ -1,15 +1,41 @@
 '''
-features = ['SAT_AVG_ALL']
+========================================
+Student model
+========================================
 
 Test loss: 49939391.2836
 Features: ['SAT_AVG_ALL']
 Number of examples: 9826
 
-features = ['SAT_AVG_ALL', 'SATVRMID', 'SATMTMID', 'SATWRMID']
-
 Test loss: 68742027.3115
 Features: ['SAT_AVG_ALL', 'SATVRMID', 'SATMTMID', 'SATWRMID']
 Number of examples: 3253
+
+Test loss: 51394117.5950
+Features: ['SAT_AVG_ALL', 'SATVRMID', 'SATMTMID']
+Number of examples: 8015
+
+Test loss: 56046866.9405
+Features: ['SATVRMID', 'SATMTMID']
+Number of examples: 8016
+
+========================================
+School model
+========================================
+
+Layer (type)                 Output Shape              Param #
+=================================================================
+dense_1 (Dense)              (None, 1)                 8
+=================================================================
+Total params: 8
+Trainable params: 8
+Non-trainable params: 0
+_________________________________________________________________
+None
+Test loss: 62799035.3775
+Features: ['ADM_RATE_ALL', 'AVGFACSAL', 'TUITIONFEE_IN', 'TUITIONFEE_OUT', 'PFTFAC', 'ICLEVEL', 'GRADS']
+Number of examples: 8396
+Batch size: 200, learning rate: 0.5000
 
 '''
 
@@ -18,20 +44,27 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 # from keras.regularizers import l2
 # from keras.utils import np_utils
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 # import seaborn as sns
 import keras
 import pandas
 import numpy as np
 import glob
 import os
+import argparse
+from keras.models import model_from_json
+# import h5py
+from sklearn import linear_model
+from sklearn.metrics import r2_score
+from sklearn.externals import joblib
 
 
-def get_clean_data(column):
+def get_clean_data(column, datatype='float'):
     # Replace bad values with -1
     column.replace('PrivacySuppressed', -1, inplace=True)
     column.fillna(-1, inplace=True)
-    return column.values.astype(np.float)
+    values = column.values.astype(np.float) if datatype == 'float' else column.values
+    return values
 
 
 def print_overall_average(file, data, name):
@@ -39,6 +72,33 @@ def print_overall_average(file, data, name):
     overall_aver = np.average(aver) if aver.size > 0 else 'No data'
     print("Year: %s, Average %s: %s, Data points: %d" %
           (file.split('/')[-1][6:-7], name, overall_aver, aver.size))
+
+
+def print_death_rate(df, year):
+    # Death rate
+    dd = {'death_rate': get_clean_data(df['DEATH_YR2_RT']), 'name': get_clean_data(df['INSTNM'], datatype='')}
+    dd_df = pandas.DataFrame(dd)
+    dd_df.sort_values('death_rate', ascending=False, inplace = True)
+    print("Year: %s" % year)
+    print("Death rate")
+    print(dd_df.head(50))
+
+
+def load_model(name, type):
+    if type == 'keras':
+        json_file = open('%s.json' % name, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights("%s.h5" % name)
+        loaded_model.compile(loss='mse', optimizer='adam')
+        print("Loaded student model from disk")
+    elif type == 'sklearn':
+        loaded_model = joblib.load('%s.pkl' % name)
+    else:
+        loaded_model = None
+    return loaded_model
 
 
 def get_data(features):
@@ -64,6 +124,9 @@ def get_data(features):
         female_earnings = get_clean_data(df['MN_EARN_WNE_MALE0_P6'])
         # Female count
         female_count = get_clean_data(df['COUNT_WNE_MALE0_P6'])
+
+        # Death rate
+        # print_death_rate(df, year)
 
         # Average earnings (female and male)
         average_earnings = []
@@ -95,7 +158,7 @@ def get_data(features):
     return data_filtered
 
 
-def train_model(features):
+def train_model(features, model_name, batch=16, n_epochs=300, learning_rate=0.1, model_type='keras'):
     n_features = len(features)
 
     dataset = get_data(features)
@@ -106,38 +169,126 @@ def train_model(features):
         else:
             data = np.concatenate((data, value), axis=0)
 
-    # data = dataset['2013_14']
-    X, Y = data[:, :n_features], data[:, -1:]
+    # Load student model if building a school model
+    if model_name == 'student':
+        X, Y = data[:, :n_features], data[:, -1:]
+    else:
+        # Load student model
+        loaded_model = load_model('student', model_type)
+
+        X_student_features = data[:, :3]
+        X = data[:, 3:n_features]
+        Y_raw = data[:, -1:]
+
+        Y_student_predicted = loaded_model.predict(X_student_features)
+        Y = np.divide(Y_raw, Y_student_predicted)
 
     # Make test and train set
     train_X, test_X, train_y, test_y = train_test_split(X, Y, train_size=0.7, random_state=0)
 
     # Linear regression
-    model = Sequential()
-    model.add(Dense(1, input_shape=(n_features,), activation="linear"))
-    model.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=0.1))
-    print(model.summary())
 
-    # train
-    history = model.fit(train_X, train_y,
-                        batch_size=16,
-                        epochs=300,
-                        verbose=1,
-                        validation_data=(test_X, test_y))
+    if model_type == 'keras':
+        # fix random seed for reproducibility
+        seed = 7
+        np.random.seed(seed)
 
-    # Evaluate and print MSE
-    score = model.evaluate(test_X, test_y, verbose=0)
-    print('Test loss: %.4f' % score)
+        model = Sequential()
+        model.add(Dense(1, input_shape=(len(X[0]),), kernel_initializer='normal', activation="linear"))
+        model.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=learning_rate))  #RMSprop(lr=0.05))  # Adam(lr=0.1)
+
+        # train
+        history = model.fit(train_X, train_y,
+                            batch_size=batch,
+                            epochs=n_epochs,
+                            verbose=1,
+                            validation_data=(test_X, test_y))
+
+        # summarize history for loss
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.draw()
+
+        # Evaluate and print MSE
+        score = model.evaluate(test_X, test_y, verbose=0)
+        print("Test loss: %.4f" % score)
+
+        # serialize model to JSON
+        model_json = model.to_json()
+        with open("%s.json" % model_name, "w") as json_file:
+            json_file.write(model_json)
+        # serialize weights to HDF5
+        model.save_weights("%s.h5" % model_name)
+        print("Saved model to disk")
+    elif model_type == 'sklearn':
+        model = linear_model.LinearRegression()
+        model.fit(train_X, train_y)
+        joblib.dump(model, '%s.pkl' % model_name)
+    else:
+        print("Specify model type!")
+        return
+
+    predictions = model.predict(test_X)
+    r_squared = r2_score(test_y, predictions)
+
+    if model_type == 'keras':
+        print("Model weights: %s" % model.get_weights())
+        print(model.summary())
+
+    elif model_type == 'sklearn':
+        # The coefficients
+        print('Coefficients: \n', model.coef_)
+
+    print("R squared: %.4f" % r_squared)
     print("Features: %s" % features)
     print("Number of examples: %d" % len(Y))
+    print("Batch size: %d, learning rate: %.4f" % (batch, learning_rate))
 
     # # Regression plot
     # ax = sns.regplot(x='X', y='Y', data=pandas.DataFrame({'X': test_X, 'Y': test_y}))
     # ax.set(xlabel='Average SAT', ylabel='Mean earnings 6 years after entry')
     # plt.show()
-
-    return score
+    return
 
 if __name__ == '__main__':
-    features = ['SAT_AVG_ALL', 'SATVRMID', 'SATMTMID'] #, 'SATWRMID']
-    score = train_model(features)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--stage', type=str, required=True)
+    parser.add_argument('--epochs', type=int, default=300)
+    parser.add_argument('--model_type', type=str, default='keras')
+    args = parser.parse_args()
+
+    features_student = [
+        'SAT_AVG_ALL',  # Average SAT
+        'SATVRMID',
+        'SATMTMID',
+        # 'SATMT75',
+        # 'SATVR75',
+        # 'SATMT25',
+        # 'SATVR25'
+    ]
+
+    features_college = [
+        'ADM_RATE_ALL',  # admission rate
+        'AVGFACSAL',  # faculty salary
+        'TUITIONFEE_IN',  # In-state tuition and fees
+        'TUITIONFEE_OUT',  # Out-of-state tuition and fees
+        'PFTFAC',  # Proportion of faculty that is full-time
+        'ICLEVEL',  # Level of institution
+        'GRADS',  # Number of graduate students
+        'CONTROL',  # Control of institution (public, private nonprofit, public for profit)
+    ]
+
+    if args.stage == 'student':
+        # Student model
+        train_model(features_student, model_name=args.stage, batch=16, n_epochs=args.epochs,
+                    learning_rate=0.1, model_type=args.model_type)
+    elif args.stage == 'school':
+        # School model
+        features = features_student + features_college
+        train_model(features, model_name=args.stage, batch=200, n_epochs=args.epochs,
+                    learning_rate=0.001, model_type=args.model_type)
+    plt.show()
